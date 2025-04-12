25d7289e4127d8c29b6228f290937b4ea9df7a91
Thomas Broyer <t.broyer@ltgt.net>
2025-04-12 19:26:46 +0200

    Update ESLint config following (new) best practices

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,29 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import babelParser from "@babel/eslint-parser";
 
-export default [
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ["packages/*/harness/**/*.js", "**/test.js", "**/*.test.js"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
   {
     files: ["packages/*/lit/**/*.js", "packages/*/vanilla/**/*.js"],
     languageOptions: {
@@ -17,13 +38,4 @@ export default [
       },
     },
   },
-  js.configs.recommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.mocha,
-      },
-    },
-  },
-];
+]);
